c73d245024cbabc405a089c0589fea93d3537f96
drakonorozdeniy <vipdady@mail.ru>
2020-10-27 16:00:34 +0300

    Добавил задания с++

---
+++ Practice/01/C++/09.Встреча/9.Встреча/9.Встреча.cpp
@@ -0,0 +1,20 @@
+﻿using namespace std;
+#include <iostream>
+int main()
+{
+    setlocale(0, "");
+    int a, b,m,c,d,p;
+    char h,l;
+    cin >> a >> h >> b;
+    cin >> c >> l >> d;
+    m = a * 60 + b;
+    p = c * 60 + d;
+    if (p - m <= 15) {
+        cout << "Встреча состоиться" << endl; 
+    }
+    else {
+        cout << "Встреча не состоиться" << endl; 
+    }
+    return 0;
+}
+
---
+++ Practice/01/C++/10.Фиксированная сумма/9. Встреча/9. Встреча.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int s,l1,r1,l2,r2,m,o ;
+	cin >> s >> l1 >> r1 >> l2 >> r2;
+	m = s - l1;
+	o = s - r1;
+	if (l2 <= m && m<= r2) {
+		cout << l1 << " " << m << endl;
+	}
+	else if (l2 <= o &&  m<= r2) {
+		cout<< r1 <<" "<<  o << endl;
+	}
+	else {
+		cout << "-1" << endl; 
+
+	}
+	
+
+	return 0;
+}
+
