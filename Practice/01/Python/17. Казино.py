20d6c59db3de296f7023349e4d77be1ca6e28676
drakonorozdeniy <vipdady@mail.ru>
2020-10-30 12:53:31 +0300

    Была предпринята попытка решения 17 задачи

---
+++ Practice/01/Python/17. Казино.py
@@ -0,0 +1,18 @@
+a=[]
+b=[0]*120
+m=[]
+n=int(input())
+while n>=0 : 
+    m.append(n)
+    n=int(input())
+    for i in range (37) :
+        p=[j for j, x in enumerate(m) if x == i
+    if len(p)>=2 :
+        a.append(m[i])
+    print(a)
+print(m)
+print(a)
+
+        
+        
+    
