6ff6f9c35cde9d38ea0bd7de9627899e2bae22eb
drakonorozdeniy <vipdady@mail.ru>
2020-11-28 11:28:54 +0300

    переделал отчет

--- Lab/01/9999.py
+++ Lab/01/9999.py
@@ -12,12 +12,9 @@ root = Tk ()
 root.title("Погода")
 root.geometry('134x189+200+100')
 
-<<<<<<< HEAD
 one =Label(root,text="Симферополь\n"+a, width=40,  font=("Ubuntu",8,BOLD), bg="#ffba00",  fg="black")
-=======
-one =Label(root,text="Симферополь\n"+a, width=40,  font=("Ubuntu",7,BOLD), bg="#ffba00",  fg="black")
->>>>>>> b31ebd05c4de1558e422e4bf32ea71eae71402dc
 one.pack()
+
 two=Label(root, text =b+"°C" , width=30,heigh=2 ,font=(" Droid Serif",40),bg="white", fg="black")
 two.pack()
 
